941cc76843fa81fb259930134a4782b6f5715433
BhanuChandar2508 <polojubhanuchandar667@gmail.com>
2026-06-04 16:28:33 +0530

    Modified GROQ API KEY access from streamlit

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 ## Load GROQ API KEy:
-os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
-groq_api_key = os.getenv("GROQ_API_KEY")
-#groq_api_key = ST.secrets["GROQ_API_KEY"]
+#os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
+#groq_api_key = os.getenv("GROQ_API_KEY")
+groq_api_key = ST.secrets["GROQ_API_KEY"]
 llm = ChatGroq(groq_api_key=groq_api_key,model="llama-3.1-8b-instant")
 
 prompt = ChatPromptTemplate.from_template(
