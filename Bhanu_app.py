f7920a1ca4b6e16c20ba5330dd9a2cb0cf7d0c3f
BhanuChandar2508 <polojubhanuchandar667@gmail.com>
2026-06-05 15:40:41 +0530

    Full Functional AI ChatBot

--- Bhanu_app.py
+++ Bhanu_app.py
@@ -35,7 +35,7 @@ from duckduckgo_search import DDGS
 load_dotenv()
 
 #groq_api_key = os.getenv("GROQ_API_KEY")
-groq_api_key = ST.secrets["GROQ_API_KEY"]
+groq_api_key = st.secrets["GROQ_API_KEY"]
 
 llm = ChatGroq(
     model="llama-3.1-8b-instant",
